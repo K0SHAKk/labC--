3efba6a125b5fd258775b67289b3959e4186e7f2
Alex <alexeigolovkov@gmail.com>
2024-11-19 18:00:13 +0700

    /

--- build/CTestTestfile.cmake
+++ build/CTestTestfile.cmake
@@ -5,5 +5,5 @@
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
 add_test(test_main "C:/Users/k0sha/Desktop/lab1adaw/build/gtest/tests.exe")
-set_tests_properties(test_main PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/k0sha/Desktop/lab1adaw/CMakeLists.txt;36;add_test;C:/Users/k0sha/Desktop/lab1adaw/CMakeLists.txt;0;")
+set_tests_properties(test_main PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/k0sha/Desktop/lab1adaw/CMakeLists.txt;34;add_test;C:/Users/k0sha/Desktop/lab1adaw/CMakeLists.txt;0;")
 subdirs("gtest")
